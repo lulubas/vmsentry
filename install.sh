eb3349375783b6ea3dcdf874a046a2993847a9c8
root <root@kv4.obambu.com>
2023-07-20 19:16:09 +0200

    MVP of VMsentry

---
+++ install.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+
+echo "Starting installation of VMsentry..."
+
+# Perform initial checks
+initial_checks() {
+    #Check for root privileges
+    if [ "$EUID" -ne 0 ]; then 
+        echo "Please run as root"
+        exit
+    fi
+    echo "Script running as root."
+
+    # Check if VMsentry already installed
+    SCRIPT_DIR="/etc/vmsentry"
+    LOG_DIR="/etc/vmsentry/logs"
+    LOG_FILE="$LOG_DIR/install.log"
+    
+    if [ -d "$SCRIPT_DIR" ]; then
+        echo "One or more VMsentry directories already exist. Aborting installation..."
+        exit 1
+    else
+        echo "VMsentry does not exist on the system. Pursuing installation..."
+    fi
+
+    #Detecting system OS
+    OS=""
+    {
+        if [ -f /etc/os-release ]; then
+            . /etc/os-release
+            OS=$NAME
+            echo "OS detected from /etc/os-release: $OS"
+        elif command -v lsb_release >/dev/null 2>&1; then
+            OS=$(lsb_release -si)
+            echo "OS detected from lsb_release: $OS"
+        elif [ -f /etc/debian_version ]; then
+            OS=Debian
+            echo "OS detected from /etc/debian_version: $OS"
+        elif [ -f /etc/redhat-release ]; then
+            OS=Redhat
+            echo "OS detected from /etc/redhat-release: $OS"
+        else
+            OS=$(uname -s)
+            echo "OS detected from uname: $OS"
+        fi
+    } | tee -a $LOG_FILE
+
+    # Check if the OS is supported, if not exit
+    if ! [[ "$OS" == "Ubuntu" || "$OS" == "Debian" || "$OS" == "CentOS Linux" || "$OS" == "AlmaLinux" || "$OS" == "Red Hat Enterprise Linux" ]]; then
+        echo "OS not supported: $OS. Exiting." | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+install_script() {
+    # Create necessary directories
+    mkdir -p $SCRIPT_DIR >/dev/null 2>&1 || { echo "Failed to create $SCRIPT_DIR" | tee -a $LOG_FILE ; exit 1; }
+    mkdir -p $LOG_DIR >/dev/null 2>&1 || { echo "Failed to create $LOG_DIR" | tee -a $LOG_FILE ; exit 1; }
+    echo "Required directories created successfully" | tee -a $LOG_FILE
+
+    # Set up logging
+    exec > >(tee -i $LOG_FILE)
+    exec 2>&1
+    echo "Logging setup successfully" | tee -a $LOG_FILE
+
+     # Check if unzip is installed and install it if it's not
+    if ! command -v unzip &> /dev/null; then
+        echo "Unzip could not be found" | tee -a $LOG_FILE
+        echo "Attempting to install unzip..." | tee -a $LOG_FILE
+        if [ "$OS" == "Ubuntu" ] || [ "$OS" == "Debian" ]; then
+            apt-get install unzip -y >/dev/null 2>&1 || { echo "Failed to install unzip on $OS" | tee -a $LOG_FILE ; exit 1; }
+        elif [ "$OS" == "CentOS" ] || [ "$OS" == "Redhat" ]; then
+            yum install unzip -y >/dev/null 2>&1 || { echo "Failed to install unzip on $OS" | tee -a $LOG_FILE ; exit 1; }
+        else
+            echo "OS not supported for automatic unzip installation" | tee -a $LOG_FILE
+            exit 1
+        fi
+        echo "Unzip installed successfully" | tee -a $LOG_FILE
+    fi
+
+    # Download the .zip file of the repository
+    echo "Downloading VMsentry" | tee -a $LOG_FILE
+    wget https://github.com/lulubas/vmsentry/archive/refs/heads/main.zip -O /etc/vmsentry/vmsentry.zip >/dev/null 2>&1 || { echo "Failed to download VMsentry" | tee -a $LOG_FILE ; exit 1; }
+
+    # Unzip the downloaded file
+    echo "Unzipping VMsentry archive..." | tee -a $LOG_FILE
+    unzip /etc/vmsentry/vmsentry.zip -d /etc/vmsentry/ >/dev/null 2>&1 || { echo "Failed to unzip VMsentry archive" | tee -a $LOG_FILE ; exit 1; }
+
+    # Remove the downloaded .zip file
+    echo "Removing installation archive..." | tee -a $LOG_FILE
+    rm /etc/vmsentry/vmsentry.zip || { echo "Failed to remove VMsentry archive" | tee -a $LOG_FILE ; exit 1; }
+    echo "VMsentry installation archive successfully removed" | tee -a $LOG_FILE
+
+    echo "VMsentry downloaded and unzipped successfully" | tee -a $LOG_FILE
+}
+
+# Check and Install Python function
+install_python() {
+    if command -v python3 &>/dev/null; then
+        echo "Python 3 is already installed"
+    else
+        echo "Python 3 is not yet installed. Starting installation now..."
+        if [[ "$OS" == "Ubuntu" || "$OS" == "Debian" ]]; then
+            apt-get update | tee -a $LOG_FILE || { echo 'Updating packages failed' | tee -a $LOG_FILE ; exit 1; }
+            apt-get install python3 -y | tee -a $LOG_FILE || { echo 'Python3 installation failed' | tee -a $LOG_FILE ; exit 1; }
+        elif [[ "$OS" == "CentOS Linux" || "$OS" == "AlmaLinux" || "$OS" == "Red Hat Enterprise Linux" ]]; then
+            yum install python3 -y | tee -a $LOG_FILE || { echo 'Python3 installation failed' | tee -a $LOG_FILE ; exit 1; }
+        else
+            echo "Python installation not supported on this OS"
+            exit 1
+        fi
+        echo "Python 3 has been installed."
+    fi
+}
+
+# Check and Install Postfix if no MTA is currently installed
+install_mta() {
+    if command -v sendmail &>/dev/null; then
+        echo "sendmail is already installed. Continuing"
+    elif command -v exim &>/dev/null; then
+        echo "Exim is already installed. Continuing"
+    elif command -v postfix &>/dev/null; then
+        echo "Postfix is already installed. Continuing"
+    else
+        echo "No MTA detected. Do you want to install Postfix? (y/n)"
+        read answer
+        if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+            if [[ "$OS" == "Ubuntu" || "$OS" == "Debian" ]]; then
+                apt-get install postfix -y | tee -a $LOG_FILE || { echo 'Postfix installation failed. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+            elif [[ "$OS" == "CentOS Linux" || "$OS" == "AlmaLinux" || "$OS" == "Red Hat Enterprise Linux" ]]; then
+                yum install postfix -y | tee -a $LOG_FILE  || { echo 'Postfix installation failed. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+            else
+                echo "Postfix installation not supported on this OS"
+                exit 1
+            fi
+            echo "Installation of Postfix successfull" | tee -a $LOG_FILE
+        else
+            echo "Installation cancelled. Exiting." | tee -a $LOG_FILE
+            exit 1
+        fi
+    fi
+}
+
+# Setup iptables and required chains
+setup_iptables() {
+    # Install iptables
+    if command -v iptables &>/dev/null; then
+        echo "iptables is already installed. Continuing" | tee -a $LOG_FILE
+    else
+        echo "iptables is not yet installed. Starting to install now." | tee -a $LOG_FILE
+        if [[ "$OS" == "Ubuntu" || "$OS" == "Debian" ]]; then
+            apt-get install iptables -y | tee -a $LOG_FILE || { echo 'Iptables installation failed. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+        elif [[ "$OS" == "CentOS Linux" || "$OS" == "AlmaLinux" || "$OS" == "Red Hat Enterprise Linux" ]]; then
+            yum install iptables -y | tee -a $LOG_FILE || { echo 'IPtables installation failed. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+        else
+            echo "iptables installation not supported on this OS"
+            exit 1
+        fi
+        echo "iptables installation successfull" | tee -a $LOG_FILE
+    fi
+
+    # Seting up logging of port 25 traffic via LOG_ONLY chain
+    echo "Setting up port 25 monitoring" | tee -a $LOG_FILE
+    iptables -N LOG_ONLY >/dev/null 2>&1 || { echo 'LOG_ONLY chain already exists. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+    iptables -A LOG_ONLY -j LOG --log-prefix "[VMS#0] Logged: " --log-level 4 || { echo 'Failed to add LOG rule to LOG_ONLY chain. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+    iptables -A LOG_ONLY -j ACCEPT || { echo 'Failed to add ACCEPT rule to LOG_ONLY chain. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+    echo "LOG_ONLY chain created and LOG and ACCEPT rules added" | tee -a $LOG_FILE
+
+    MAIN_IFACE=$(ip route show default | awk '/default/ {print $5}' | sed -n 2p)
+    echo "Main network interface detected: $MAIN_IFACE" | tee -a $LOG_FILE
+    iptables -I FORWARD -o $MAIN_IFACE -p tcp --dport 25 -j LOG_ONLY || { echo 'Failed to add LOG_ONLY rule to FORWARD chain. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+    echo "Port 25 traffic redirected to LOG_ONLY chain" | tee -a $LOG_FILE
+
+    # Setuping up the LOG_AND_DROP chain and rules
+    echo "Creating LOG_AND_DROP chain and rules" | tee -a $LOG_FILE
+    iptables -N LOG_AND_DROP >/dev/null 2>&1 || { echo 'LOG_AND_DROP chain already exists. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+    iptables -A LOG_AND_DROP -j LOG --log-prefix "[VMS#1] Dropped: " --log-level 4 || { echo 'Failed to add LOG rule to LOG_AND_DROP chain. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+    iptables -A LOG_AND_DROP -j DROP || { echo 'Failed to add DROP rule to LOG_AND_DROP chain. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+    echo "iptables LOG_AND_DROP chain and rules created successfully" | tee -a $LOG_FILE
+
+    # Change the log location
+    echo -e ':msg, startswith, "VMS#0" -/etc/vmsentry/logs/iptables_all_25.log\n:msg, startswith, "VMS#1" -/etc/vmsentry/logs/iptables_dropped_25.log' > /etc/rsyslog.d/vms_iptables.conf | tee -a $LOG_FILE || { echo 'Failed to edit VMS log location in rsyslog. Exiting.' | tee -a $LOG_FILE ; exit 1; }
+    echo "Rsyslog configuration file updated to redirect iptables outgoing port 25 logs" | tee -a $LOG_FILE
+    echo "Restarting Rsyslog..." | tee -a $LOG_FILE
+    systemctl restart rsyslog | tee -a $LOG_FILE || { echo 'Failed to restart rsyslog.' | tee -a $LOG_FILE ; exit 1; }
+    echo "Rsyslog restarted successfully" | tee -a $LOG_FILE
+}
+
+# Setup cron job to run every X minutes
+setup_cron() {
+    # Name of the cron job for easy identification
+    CRON_NAME="VMsentry"
+
+    # Check if the cron job already exists
+    if crontab -l | grep -q "$CRON_NAME"; then
+        echo "Cron job already exists. Skipping addition." | tee -a $LOG_FILE
+    else
+        # Cron job does not exist, add it
+        echo "Adding cron job..." | tee -a $LOG_FILE
+        # Write out current crontab to a temp file
+        crontab -l > mycron || { echo "Failed to write current crontab to file. Exiting." | tee -a $LOG_FILE ; exit 1; }
+        # Echo new cron into cron file, run script every 10 minutes
+        echo "*/10 * * * * /usr/bin/python3 /etc/vmsentry/vm_sentry.py # $CRON_NAME" >> mycron
+        # Install new cron file
+        crontab mycron || { echo "Failed to install new crontab. Exiting." | tee -a $LOG_FILE ; exit 1; }
+        rm mycron
+        echo "Cron job added successfully." | tee -a $LOG_FILE
+    fi
+}
+
+initial_checks
+install_script
+install_python
+install_mta
+setup_iptables
+setup_cron
+
+echo "Install finished. Please check $LOG_FILE for the log"
---
+++ vm_sentry.py
@@ -0,0 +1,245 @@
+import subprocess
+import configparser
+import re
+import collections
+from datetime import datetime, timedelta
+import logging
+import sys
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# Setting up the log file
+def setup_logging():
+    logging.basicConfig(filename='logs/vmsentry.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
+# Loading configration file and variables
+def load_config():
+    logging.info('Loading Configuration file')
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    try:
+        timeframe = int(config.get('settings', 'timeframe'))
+        smtp_threshold = int(config.get('settings', 'smtp_threshold'))
+        unique_ips_threshold = int(config.get('settings', 'unique_ips_threshold'))
+        mode = config.get('settings', 'mode')
+        hash_limit_min = int(config.get('settings', 'hash_limit_min'))
+        hash_limit_burst = int(config.get('settings', 'hash_limit_burst'))
+        from_addr = config.get('email', 'from_addr')
+        to_addr = config.get('email', 'to_addr')
+        send_mail = config.getboolean('email', 'send_email')
+
+        # Check if values are positive for certain parameters
+        if not all(value > 0 for value in [timeframe, smtp_threshold, unique_ips_threshold, hash_limit_min, hash_limit_burst]):
+            raise ValueError("One or more config values are not positive integers.")
+
+        # Check if mode is valid
+        if mode not in ['monitor', 'block', 'limit']:
+            raise ValueError("Invalid mode in configuration file.")
+
+        # Check if email addresses are not empty
+        if not all(addr for addr in [from_addr, to_addr]):
+            raise ValueError("One or more email addresses are missing.")
+
+    except (configparser.NoOptionError, configparser.NoSectionError, ValueError) as e:
+        logging.error(f"Error in configuration file: {str(e)}. Exiting")
+        sys.exit(1)
+    
+    except Exception as e:
+        logging.error(f"Unexpected error: {str(e)}. Exiting")
+        sys.exit(1)
+
+    return timeframe, smtp_threshold, unique_ips_threshold, mode, hash_limit_min, hash_limit_burst, from_addr, to_addr, send_mail
+
+# Fetch the list of all guest VMs
+def get_vms():
+
+    vms_output = subprocess.check_output('virsh list --all | grep -v -E \'^-|Id|^$\' | awk \'{print $2}\'', shell=True)
+    vms = vms_output.decode().split('\n')
+    vms = list(filter(None, vms))
+
+    return vms
+
+# Trim the list of VMs with only the running ones
+def get_running_vms(vms):
+
+    running_vms = []
+
+    for vm in vms:
+        try:
+            state_output = subprocess.check_output('virsh domstate {}'.format(vm), shell=True)
+            state = state_output.decode().strip()
+
+            if state == 'running':
+                running_vms.append(vm)
+
+        except Exception as e:
+            logging.error(f'Error fetching status for VM {vm}: {str(e)}. Skipping.')
+            return
+            
+    return running_vms
+
+# Associate VMs to their IP address
+def get_vm_ips(running_vms):
+
+    vm_ips = {}
+
+    for vm in running_vms:
+        try:
+            mac_output = subprocess.check_output('virsh domiflist {} | awk \'{{ print $5}}\' | tail -2 | head -1'.format(vm), shell=True)
+            mac = mac_output.decode().strip()
+            ip_output = subprocess.check_output('ip neigh | grep {} | awk \'{{print $1}}\''.format(mac), shell=True)
+            ip = ip_output.decode().strip()
+            vm_ips[vm] = ip
+
+        except Exception as e:
+            logging.error(f'Error fetching IP for VM {vm}: {str(e)}. Skipping.')
+            return
+    
+    if len(running_vms) != len(vm_ips):
+        logging.error(f"Not all VMs have an associated IP address. VM: {len(running_vms)}, VMs with IP: {len(vm_ips)}. Exiting")
+        sys.exit(1)
+
+    return vm_ips
+
+# Parse iptables logs and extract SMTP connexions per IP and unique destination IPs  
+def parse_logs(timeframe_hours):
+    try:
+        with open("logs/iptables_all.log") as f:
+            lines = f.readlines()
+        if not lines:
+            logging.error('iptables_all.log file is empty. Please wait until logs start being generated. Exiting')
+            sys.exit(1)
+    except FileNotFoundError:
+        logging.error('iptables_all.log file does not exist. Exiting.')
+        sys.exit(1)
+    except Exception as e:
+        logging.error(f'Unexpected error when reading iptables_all.log: {str(e)}. Exiting.')
+        sys.exit(1)
+
+    pattern = r"SRC=(?P<src>\S+) DST=(?P<dst>\S+) .* DPT=25"
+    timeframe = datetime.now() - timedelta(hours=timeframe_hours)
+    connections = collections.defaultdict(int)
+    unique_ips = collections.defaultdict(set)
+
+    for line in lines:
+        try:
+            timestamp_str, log_message = line.split("kernel:", 1)
+            timestamp_str = timestamp_str.strip().rsplit(" ", 1)[0]
+            timestamp = datetime.strptime(timestamp_str + ' ' + str(datetime.now().year), "%b %d %H:%M:%S %Y")
+
+            if timestamp > timeframe:
+                match = re.search(pattern, log_message)
+                if match:
+                    src = match.group("src")
+                    dst = match.group("dst")
+                    connections[src] += 1
+                    unique_ips[src].add(dst)
+        except Exception as e:
+            logging.error(f'Unexpected error while parsing the line "{line}": {str(e)}. Skipping.')
+            continue
+
+    return connections, unique_ips
+
+# Compare the gathered data with the pre-defined thresholds and take the predetermined action
+def handle_ip(mode, connections, unique_ips, smtp_threshold, unique_ips_threshold, hash_limit_min, hash_limit_burst, from_addr, to_addr, send_mail):
+    if is_chain_exists('LOG_AND_DROP'):
+        for ip in connections.keys():
+            if connections[ip] > smtp_threshold or len(unique_ips[ip]) > unique_ips_threshold and not is_ip_blocked(ip):
+                if mode == 'monitor':
+                    logging.info(f"[Monitor] Thresholds reached for {ip} -- Connections: {connections[ip]} -- Unique IPs: {len(unique_ips[ip])}")
+                elif mode == 'block':
+                    block_ip(ip)
+                elif mode == 'limit':
+                    limit_ip(ip, hash_limit_min, hash_limit_burst)
+                if send_mail:
+                    send_notification(ip, mode, connections, unique_ips, from_addr, to_addr)
+
+# Send a mail notification when the threshold is reached
+def send_notification(ip, mode, connections, unique_ips, from_addr, to_addr):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = from_addr
+        msg['To'] = to_addr
+        msg['Subject'] = f'VMsentry Alert: IP {ip} exceeded threshold'
+        
+        body = f'IP: {ip}\nSMTP Connections: {connections[ip]}\nUnique IPs: {unique_ips[ip]}\nMode of action: {mode}'
+        msg.attach(MIMEText(body, 'plain'))
+
+        server = smtplib.SMTP('localhost')
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+        server.quit()
+
+        logging.info(f"Notification sent to {to_addr} for IP {ip}")
+
+    except Exception as e:
+        logging.error(f"Error sending notification: {str(e)}")
+
+# Check if the iptables chain exists
+def is_chain_exists(chain):
+    try:
+        subprocess.check_output(f'iptables -L {chain} -n', shell=True)
+        return True
+    except subprocess.CalledProcessError:
+        logging.error(f'{chain} chain does not exist. Exiting')
+        raise
+
+# Check if the IP address is already blocked or limited in iptables
+def is_ip_blocked(ip):
+    try:
+        iptables_rules_output = subprocess.check_output('iptables -L -n', shell=True)
+        iptables_rules = iptables_rules_output.decode()
+        if f' {ip} ' in iptables_rules:
+            logging.info(f"IP {ip} is already blocked or limited.")
+            return True
+        else:
+            return False
+    except Exception as e:
+        logging.error(f"Error checking if IP {ip} is blocked: {str(e)}. Exiting")
+        raise
+
+# Block IP address port 25 access when the threshold is reached
+def block_ip(ip):
+    try:
+        block_command = f'iptables -I FORWARD 2 -p tcp --dport 25 -s {ip} -j LOG_AND_DROP'
+        subprocess.run(block_command, shell=True)
+        logging.info(f"IP {ip} blocked.")
+    except Exception as e:
+        logging.error(f"Error blocking IP {ip}: {str(e)}")
+
+# Limit IP address port 25 access when the threshold is reached
+def limit_ip(ip, hash_limit_min, hash_limit_burst):
+    try:
+        block_command_1 = f'iptables -I FORWARD 2 -s {ip} -p tcp --dport 25 -j LOG_AND_DROP'
+        block_command_2 = f'iptables -A FORWARD 2 -s {ip} -p tcp --dport 25 -m hashlimit --hashlimit {hash_limit_min}/min --hashlimit-burst {hash_limit_burst} --hashlimit-mode srcip --hashlimit-name smtp_limit -j ACCEPT'
+        subprocess.run(block_command_1, shell=True)
+        subprocess.run(block_command_2, shell=True)
+        logging.info(f"IP {ip} access to port 25 has been rate limited.")
+    except Exception as e:
+        logging.error(f"Error limiting IP {ip}: {str(e)}")
+
+# Main function
+def main():
+
+    logging.info("Starting to run vmsentry...")
+    setup_logging()
+    timeframe, smtp_threshold, unique_ips_threshold, mode, hash_limit_min, hash_limit_burst, from_addr, to_addr, send_mail = load_config()
+    logging.info("Config.ini file successfully loaded")
+
+    logging.info("Fetching VM names and IP addresses")
+    vms = get_vms()
+    running_vms = get_running_vms(vms)
+    vm_ips = get_vm_ips(running_vms)
+    logging.info(f'Fetching successfull. Total VPS: {len(vms)}, {len(running_vms)} running')
+
+    logging.info(f"Parsing logs file more recent than {timeframe} hours")
+    connections, unique_ips = parse_logs(timeframe)
+    logging.info("Logs parsed successfully")
+
+    logging.info("Taking actions against IP addresses over quotas...")
+    handle_ip(mode, connections, unique_ips, smtp_threshold, unique_ips_threshold, hash_limit_min, hash_limit_burst, from_addr, to_addr, send_mail)
+    logging.info("All actions have now been taken. Program run successfull. Exiting")
+
+if __name__ == '__main__':
+    main()
