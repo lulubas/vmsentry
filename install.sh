aa9c7c98d1562d6a4335c068ba200bd5576f87e7
root <root@kv4.obambu.com>
2023-07-27 23:49:13 +0200

    fixing installation problem with cron wrapper

--- install.sh
+++ install.sh
@@ -91,6 +91,8 @@ install_script() {
     echo "Unzipping VMsentry archive..." | tee -a $LOG_FILE
     unzip -j /etc/vmsentry/vmsentry.zip -d /etc/vmsentry/ >/dev/null 2>&1 || { echo "Failed to unzip VMsentry archive" | tee -a $LOG_FILE ; exit 1; }
     mkdir /etc/vmsentry/cron/ || { echo "Failed to create cron directory" | tee -a $LOG_FILE ; exit 1; }
+    ls /etc/vmsentry/run_vmsentry.sh
+    pwd
     mv /etc/vmsentry/run_vmsentry.sh /etc/vmsentry/cron/ || { echo "Failed to move cron wrapper into cron directory" | tee -a $LOG_FILE ; exit 1; }
 
     # Remove the downloaded .zip file
