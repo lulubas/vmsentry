fe04dc66bdc1cc51de68879078a040f101481e9e
root <root@work.obambu.com>
2023-10-18 18:55:43 -0400

    still fixing IP unblocking

--- vm_sentry.py
+++ vm_sentry.py
@@ -320,8 +320,10 @@ def expire_ip(block_timelimit):
             timestamp = parse_time(line)
 
             if timestamp > timeframe:
+                print(f"timestamp for {ip}")
                 match = re.search(pattern, line)
                 if match:
+                    print(f"match for {ip}")
                     ip = match.group("ip")
                     unblock_ip(ip)
                     remove_line_from_file(log_file_path, line)  # remove the line from the file
@@ -341,6 +343,7 @@ def remove_line_from_file(filename, line_to_remove):
                 f.write(line)
 
 def unblock_ip(ip):
+    print(f"unblock for {ip}")
     rule_number = is_ip_blocked(ip)
     try:
         block_command = f'iptables -D OUTGOING_MAIL {rule_number}'
